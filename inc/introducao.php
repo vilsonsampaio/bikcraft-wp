e5f7abc926ed0fbfa8559eab9135ef539d8ed0ea
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-10-10 18:08:52 -0300

    introdução das páginas internas com custom field

--- inc/introducao.php
+++ inc/introducao.php
@@ -1,6 +1,13 @@
-<section class="introducao-interna interna_sobre">
+<style>
+	.introducao-interna {
+	background: url(<?php the_field('background_intro'); ?>) no-repeat center;
+	background-size: cover;
+}
+</style>
+
+<section class="introducao-interna">
 	<div class="container">
-		<h1>Sobre</h1>
-		<p>conheça mais sobre a bikcraft</p>
+		<h1><?php the_title(); ?></h1>
+		<p><?php the_field('subtitle_intro') ?></p>
 	</div>
 </section>
