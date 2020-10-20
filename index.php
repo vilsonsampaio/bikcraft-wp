e63d424ae824a60b3dc5fafd3834b57e8afbeeb0
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-10-19 21:53:25 -0300

    blog adicionado

--- index.php
+++ index.php
@@ -1,13 +1,29 @@
 <?php get_header(); ?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	
-	<h1><?php the_title(); ?></h1>
-	<?php the_content(); ?>
+
+<article>
+	<section class="introducao-interna introducao-geral">
+		<div class="container">
+			<h1><?php the_title(); ?></h1>
+		</div>
+	</section>
+
+	<section class="container conteudo-geral">
+		<div class="grid-8">
+			<?php the_content(); ?>
+		</div>
+	</section>
+</article>
 
 <?php endwhile; else: ?>
-<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-<?php endif; ?>
 
+	<section class="introducao-interna introducao-geral">
+		<div class="container">
+			<h1>Página não encontrada.</h1>
+		</div>
+	</section>
+
+<?php endif; ?>
 
 <?php get_footer(); ?>
